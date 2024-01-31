4190af7837d64893b53dea6d49e1dbb0d39757c8
lianghua1987 <hua_liang@mckinsey.com>
2024-02-01 02:18:05 +0800

    wrap up and add skaffold

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,9 +10,9 @@ app.post('/events', (req, res) => {
     const event = req.body;
     events.push(event);
     axios.post('http://posts-clusterip-srv:4000/events', event).catch(e => console.error(e)); // post
-    axios.post('http://localhost:4001/events', event).catch(e => console.error(e));  // comments
-    axios.post('http://localhost:4002/events', event).catch(e => console.error(e));  // query
-    axios.post('http://localhost:4003/events', event).catch(e => console.error(e));  // moderation
+    axios.post('http://comments-srv:4001/events', event).catch(e => console.error(e));  // comments
+    axios.post('http://query-srv:4002/events', event).catch(e => console.error(e));  // query
+    axios.post('http://moderation-srv:4003/events', event).catch(e => console.error(e));  // moderation
     res.send({status: 'OK'})
 });
 
