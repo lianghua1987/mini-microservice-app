55029d9d4fabf95d4351952900d72c8b4052882b
lianghua1987 <hua_liang@mckinsey.com>
2024-01-31 10:37:34 +0800

    Add all services k8s deployment

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,7 +9,7 @@ const events = [];
 app.post('/events', (req, res) => {
     const event = req.body;
     events.push(event);
-    axios.post('http://localhost:4000/events', event).catch(e => console.error(e)); // post
+    axios.post('http://posts-clusterip-srv:4000/events', event).catch(e => console.error(e)); // post
     axios.post('http://localhost:4001/events', event).catch(e => console.error(e));  // comments
     axios.post('http://localhost:4002/events', event).catch(e => console.error(e));  // query
     axios.post('http://localhost:4003/events', event).catch(e => console.error(e));  // moderation
